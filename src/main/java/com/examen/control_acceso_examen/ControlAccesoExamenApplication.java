7fc5f60789d62b1265c69e50393a546cbc6f22c5
Juanjo <juanjetesc@gmail.com>
2025-02-08 12:34:53 +0100

    Initial commit

---
+++ src/main/java/com/examen/control_acceso_examen/ControlAccesoExamenApplication.java
@@ -0,0 +1,13 @@
+package com.examen.control_acceso_examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlAccesoExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlAccesoExamenApplication.class, args);
+	}
+
+}
