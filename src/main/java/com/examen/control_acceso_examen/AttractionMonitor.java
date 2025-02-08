625969d3fada665d779a0792a7558198eb245464
Juanjo <juanjetesc@gmail.com>
2025-02-08 12:38:52 +0100

    Clase AtractionMonitor ok

--- src/main/java/com/examen/control_acceso_examen/AttractionMonitor.java
+++ src/main/java/com/examen/control_acceso_examen/AttractionMonitor.java
@@ -1,4 +1,21 @@
 package com.examen.control_acceso_examen;
 
 public class AttractionMonitor {
+    private int availableSpots;
+
+    public AttractionMonitor(int capacity) {
+        this.availableSpots = capacity;
+    }
+
+    public synchronized void enter() throws InterruptedException {
+        while (availableSpots <= 0) {
+            wait();
+        }
+        availableSpots--;
+    }
+
+    public synchronized void exit() {
+        availableSpots++;
+        notifyAll();
+    }
 }
